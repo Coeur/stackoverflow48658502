3c529d9288c94beff9362d2b3a1954d8f46dd344
Antoine Cœur <coeur@gmx.fr>
2018-02-07 20:44:19 +0800

    fixed version of the CircularLabel

--- roundBorders/CircularLabel.swift
+++ roundBorders/CircularLabel.swift
@@ -15,7 +15,24 @@ class CircularLabel: UILabel {
         layer.borderColor = UIColor.white.cgColor
         layer.borderWidth = 5
         layer.masksToBounds = true
-        layer.shadowColor = UIColor.clear.cgColor
-        clipsToBounds = true
+    }
+}
+
+/// Circular background with white border (fixed)
+class FixedCircularLabel: UILabel {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = frame.size.height / 2
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 5
+        layer.masksToBounds = true
+    }
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        // workaround incomplete borders: https://stackoverflow.com/a/48663935/1033581
+        UIColor(cgColor: layer.borderColor!).setStroke()
+        let path = UIBezierPath(roundedRect: bounds, cornerRadius: layer.cornerRadius)
+        path.lineWidth = 1
+        path.stroke()
     }
 }
